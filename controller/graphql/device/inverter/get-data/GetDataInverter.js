be30a0affa7ce3a661dfa0c3969c026a9fbaa068
gesarizky <gesarizkynugraha@gmail.com>
2023-08-04 15:29:49 +0700

    newgetrms + newgetinverter

--- controller/graphql/device/inverter/get-data/GetDataInverter.js
+++ controller/graphql/device/inverter/get-data/GetDataInverter.js
@@ -3,33 +3,61 @@ import { apolloClient } from "../../../apollo";
 import postInverterData from "../../../../device/inverter/post/PostInverterData";
 
 const getDataInverter = async () => {
-  const GET_INVERTER_SUBCRIPTION = gql`
-    subscription {
-      Inverter(order_by: { createdAt: desc }, limit: 1) {
-        id
-        data
-        UUID_User
+  try {
+    const GET_LAST_CREATED_AT = gql`
+      query GetLastCreatedAt {
+        Inverter(limit: 1, order_by: { createdAt: desc }) {
+          createdAt
+        }
       }
-    }
-  `;
-  const handleDataUpdate = (data) => {
-    // Olah data yang diperbarui di sini
-    data.map((data) => {
-      // console.log(data);
-      postInverterData(data);
+    `;
+
+    const lastCreatedAtResult = await apolloClient.query({
+      query: GET_LAST_CREATED_AT,
     });
-  };
-  const subscription = apolloClient.subscribe({
-    query: GET_INVERTER_SUBCRIPTION,
-  });
-  subscription.subscribe({
-    next: (result) => {
-      const data = result.data.Inverter;
-      handleDataUpdate(data);
-    },
-    error: (error) => {
-      console.error("Subscription error:", error);
-    },
-  });
+    let lastCreatedAt =
+      lastCreatedAtResult.data.RMS[0]?.createdAt || "1970-01-01T00:00:00.000Z"; // Jika tidak ada data, gunakan timestamp awal
+
+    const GET_INVERTER_SUBCRIPTION = gql`
+      subscription GetNewInverterData($lastSeenTimestamp: timestamptz) {
+        Inverter(where: { createdAt: { _gt: $lastSeenTimestamp } }) {
+          id
+          data
+          UUID_User
+        }
+      }
+    `;
+
+    const handleDataUpdate = (data) => {
+      const newData = data.filter(
+        (dataItem) => dataItem.createdAt > lastCreatedAt
+      );
+      newData.forEach((dataItem) => {
+        console.log("Processing new data:", dataItem);
+        console.log("Waktu yang di track", lastCreatedAt);
+        lastCreatedAt = dataItem.createdAt;
+        postInverterData(dataItem);
+      });
+    };
+
+    const subscription = apolloClient.subscribe({
+      query: GET_INVERTER_SUBCRIPTION,
+      variables: {
+        lastSeenTimestamp: lastCreatedAt,
+      },
+    });
+
+    subscription.subscribe({
+      next: (result) => {
+        const data = result.data.Inverter;
+        handleDataUpdate(data);
+      },
+      error: (error) => {
+        console.error("Subscription error:", error);
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching lastCreatedAt:", error);
+  }
 };
 export default getDataInverter;
--- controller/graphql/device/rms/get-data/GetDataRms.js
+++ controller/graphql/device/rms/get-data/GetDataRms.js
@@ -2,34 +2,69 @@ import { gql } from "@apollo/client";
 import { apolloClient } from "../../../apollo";
 import postRmsData from "@/controller/device/rms/post/PostRmsData";
 
+
 const getDataRms = async () => {
-  const GET_RMS_SUBCRIPTION = gql`
-    subscription {
-      RMS(limit: 1, order_by: { createdAt: desc }) {
-        UUID_User
-        data
-        id
+  try {
+
+    const GET_LAST_CREATED_AT = gql`
+      query GetLastCreatedAt {
+        RMS(limit: 1, order_by: { createdAt: desc }) {
+          createdAt
+        }
       }
-    }
-  `;
-  const handleDataUpdate = (data) => {
-    // Olah data yang diperbarui di sini
-    const datamap = data.map((data) => {
-      // console.log(data);
-      postRmsData(data);
+    `;
+
+    const lastCreatedAtResult = await apolloClient.query({
+      query: GET_LAST_CREATED_AT,
     });
-  };
-  const subscription = apolloClient.subscribe({
-    query: GET_RMS_SUBCRIPTION,
-  });
-  subscription.subscribe({
-    next: (result) => {
-      const data = result.data.RMS;
-      handleDataUpdate(data);
-    },
-    error: (error) => {
-      console.error("Subscription error:", error);
-    },
-  });
+
+    let lastCreatedAt =
+      lastCreatedAtResult.data.RMS[0]?.createdAt || "1970-01-01T00:00:00.000Z"; // Jika tidak ada data, gunakan timestamp awal
+
+    const GET_RMS_SUBCRIPTION = gql`
+      subscription GetNewRmsData($lastSeenTimestamp: timestamptz) {
+        RMS(where: { createdAt: { _gt: $lastSeenTimestamp } }) {
+          UUID_User
+          data
+          id
+          createdAt
+        }
+      }
+    `;
+
+    const handleDataUpdate = (data) => {
+      const newData = data.filter(
+        (dataItem) => dataItem.createdAt > lastCreatedAt
+      );
+      newData.forEach((dataItem) => {
+        console.log("Processing new data:", dataItem);
+        console.log("Waktu yang di track",lastCreatedAt);
+        lastCreatedAt = dataItem.createdAt;
+        postRmsData(dataItem);
+      });
+    };
+
+    const subscription = apolloClient.subscribe({
+      query: GET_RMS_SUBCRIPTION,
+      variables: {
+        lastSeenTimestamp: lastCreatedAt,
+      },
+    });
+
+    subscription.subscribe({
+      next: (result) => {
+        const data = result.data.RMS;
+        handleDataUpdate(data);
+      },
+      error: (error) => {
+        console.error("Subscription error:", error);
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching lastCreatedAt:", error);
+  }
 };
+
 export default getDataRms;
+
+
